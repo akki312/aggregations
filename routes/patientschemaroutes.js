9541e4215dd6b58940abcc7afbe6845d84beee8e
akki312 <akshithsistla@gmail.com>
2024-06-18 13:17:00 +0530

    patientschemaroutes.js

--- routes/patientschemaroutes.js
+++ routes/patientschemaroutes.js
@@ -38,7 +38,7 @@ router.get('/api/patientMedicines', async (req, res) => {
 });
 
 // Update patient medicine record
-router.put('/api/patientMedicines/:id', async (req, res) => {
+router.post('/api/patientMedicines/:id', async (req, res) => {
   try {
     const patientMedicine = await patientMedicineService.updatePatientMedicine(req.params.id, req.body);
     res.status(200).json(patientMedicine);
