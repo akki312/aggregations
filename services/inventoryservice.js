b5d0da23c6e0288f609661612351eb90d17b2ab6
akki312 <akshithsistla@gmail.com>
2024-06-20 13:22:44 +0530

    inventoryservice.js:

--- services/inventoryservice.js
+++ services/inventoryservice.js
@@ -186,7 +186,7 @@ async function getFinancialSummary(startDate, endDate) {
   const start = new Date(startDate);
   const end = new Date(endDate);
   
-  // Ensure the end date includes the whole day
+  
   end.setHours(23, 59, 59, 999);
 
   return Inventory.aggregate([
