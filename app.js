aa57c301e31db82096c911c24a09c301b21eea68
akki312 <akshithsistla@gmail.com>
2024-06-20 09:18:10 +0530

    app.js

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ app.use(bodyParser.json());
 
 app.use('/inventory', inventoryRoutes);
 app.use('/patients', patientRoutes);
-app
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
